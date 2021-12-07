c9e4e4d9803b4dc5db5388e779182c2b008c6f2e
samsonenkomax87 <samsonenko.max@gmail.com>
2021-12-07 15:56:57 +0200

    second task started

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,17 +2,25 @@ import flatpickr from 'flatpickr';
 // Дополнительный импорт стилей
 import 'flatpickr/dist/flatpickr.min.css';
 
+const start = Date.now();
+
 const options = {
   enableTime: true,
   time_24hr: true,
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates, dateStr, instance) {
-    console.log(
-      ...selectedDates.map(one => {
-        console.log(one);
-      }),
-    );
+    const date = new Date(selectedDates).getTime();
+    if(date > start){
+      startBtn.disabled = false;
+      const dateone = date-start;
+      console.log(dateone)
+      console.log(convertMs(dateone))
+
+    }
+
+     
+    
   },
 };
 
@@ -20,3 +28,26 @@ const startBtn = document.querySelector('button[data-start]');
 startBtn.disabled = true;
 
 flatpickr('#datetime-picker', options);
+
+
+
+console.log(start)
+
+function convertMs(ms) {
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
+
+  // Remaining days
+  const days = Math.floor(ms / day);
+  // Remaining hours
+  const hours = Math.floor((ms % day) / hour);
+  // Remaining minutes
+  const minutes = Math.floor(((ms % day) % hour) / minute);
+  // Remaining seconds
+  const seconds = Math.floor((((ms % day) % hour) % minute) / second);
+
+  return { days, hours, minutes, seconds };
+}
